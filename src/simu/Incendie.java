710fa5060dcfe8330b6fb061033339e9e597db32
Hadrien Eyraud <Hadrien.Eyraud@grenoble-inp.org>
2022-10-25 12:52:01 +0200

    fix: Case import to Incendie class

---
+++ src/simu/Incendie.java
@@ -0,0 +1,35 @@
+package simu;
+import terrain.Case;
+
+public class Incendie {
+    private Case fireCase;
+    private int nbL;
+
+    public Incendie(Case fireCase, int nbL) {
+        this.fireCase = fireCase;
+        this.nbL = nbL;
+    }
+
+    public String toString() {
+        return "Incendie{" +
+                "fireCase=" + fireCase +
+                ", nbL=" + nbL +
+                '}';
+    }
+
+    public Case getFireCase() {
+        return fireCase;
+    }
+
+    public void setFireCase(Case fireCase) {
+        this.fireCase = fireCase;
+    }
+
+    public int getNbL() {
+        return nbL;
+    }
+
+    public void setNbL(int nbL) {
+        this.nbL = nbL;
+    }
+}
