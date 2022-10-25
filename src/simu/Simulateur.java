fdfb6cdfb92c45517083ce1cfe8b29056ebd6691
Titouan Mazier <titouan.mazier@grenoble-inp.org>
2022-10-25 17:33:16 +0200

    feat: Simulateur squeleton

--- src/simu/Simulateur.java
+++ src/simu/Simulateur.java
@@ -1,2 +1,16 @@
-package simu;public class Simulateur {
+package simu;
+
+import gui.Simulable;
+
+public class Simulateur implements Simulable {
+
+    @Override
+    public void next() {
+        // TODO
+    }
+
+    @Override
+    public void restart() {
+        // TODO
+    }
 }
