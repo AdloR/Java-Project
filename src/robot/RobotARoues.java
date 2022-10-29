c143a908385dd875d89bbb56337afb70b46a1954
eyraudh <Hadrien.eyraud@grenoble-inp.org>
2022-10-29 18:06:19 +0200

    wip :robots

--- src/robot/RobotARoues.java
+++ src/robot/RobotARoues.java
@@ -1,8 +1,14 @@
 package robot;
 
 import terrain.Case;
+import terrain.NatureTerrain;
+
+import static terrain.NatureTerrain.HABITAT;
+import static terrain.NatureTerrain.TERRAIN_LIBRE;
 
 public class RobotARoues extends Robot {
+
+
     public RobotARoues(Case position) {
         this.position = position;
         this.speed = 80;
@@ -13,4 +19,10 @@ public class RobotARoues extends Robot {
         this.timeIntervention = 5;
     }
 
+    @Override
+    public void setPosition(Case position) {
+        if (position.getType() == TERRAIN_LIBRE || position.getType() == HABITAT) {
+            this.position = position;
+        }
+    }
 }
