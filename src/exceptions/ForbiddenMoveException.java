856453296f6c5d2f5ed45fcb0db2fec57274cb80
eyraudh <Hadrien.eyraud@grenoble-inp.org>
2022-10-30 18:08:36 +0100

    feat: Adding firefighterChief class + javadoc to exceptions NotNeighboringCasesException javadoc incomplete.

--- src/exceptions/ForbiddenMoveException.java
+++ src/exceptions/ForbiddenMoveException.java
@@ -1,7 +1,14 @@
 package exceptions;
 
 public class ForbiddenMoveException extends Exception{
-
+    /**
+     * Occur if you try to set the case of a robot to a case where the case nature is
+     * unreachable for the robot concerned.
+     *
+     * @param message Message printed by caught exception.
+     *
+     * @exception ForbiddenMoveException
+     */
     public ForbiddenMoveException(String message) {
         super(message);
     }
