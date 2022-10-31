eb84d0cd6503ab60a3c48bbc55ff4c1e5f0fb89f
eyraudh <Hadrien.eyraud@grenoble-inp.org>
2022-10-31 19:19:02 +0100

    refactor: Directory exception

--- src/exceptions/UnknownDirectionException.java
+++ src/exceptions/UnknownDirectionException.java
@@ -1,6 +1,10 @@
 package exceptions;
 
 public class UnknownDirectionException extends Exception{
+    /**
+     * Occur if you try to move to a direction not in {NORD, SUD, EST, OUEST}
+     * @param message Message printed by caught exception
+     */
     public UnknownDirectionException(String message) {
         super(message);
     }
