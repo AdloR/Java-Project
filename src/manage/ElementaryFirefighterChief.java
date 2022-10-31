0359a6b1fefa776ca283b7d34ac7b1a2d9665df6
AdloR <arthurrosa51@gmail.com>
2022-10-31 18:08:22 +0100

    Meilleure syntaxe itérateur Firefighter

--- src/manage/ElementaryFirefighterChief.java
+++ src/manage/ElementaryFirefighterChief.java
@@ -8,24 +8,24 @@ import terrain.Carte;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-public class ElementaryFirefighterChief extends FireFighterChief{
+public class ElementaryFirefighterChief extends FireFighterChief {
     public ElementaryFirefighterChief(ArrayList<Robot> robots, ArrayList<Incendie> incendies, Carte carte) {
         super(robots, incendies, carte);
     }
 
     public void affectRobot() {
-        Iterator<Incendie> it = incendies.iterator();
         int robotsSize = robots.size();
-        while (it.hasNext()) {
-            for (int i = 0; i < robotsSize; i++) {
-                if (robots.get(i).isWaiting()) {
-                    Path path = robots.get(i).aStar(carte, robots.get(i).getPosition(), it.next().getFireCase());
-                    robots.get(i).followPath(path);
+        for (Incendie incendie : incendies) {
+            if (incendie.getNbL() >= 0)
+                for (int i = 0; i < robotsSize; i++) {
+                    if (robots.get(i).isWaiting()) {
+                        Path path = robots.get(i).aStar(carte, robots.get(i).getPosition(), incendie.getFireCase());
+                        robots.get(i).followPath(path);
+                    }
+                    if (i == robotsSize - 1) {
+                        i = 0;
+                    }
                 }
-                if (i == robotsSize - 1) {
-                    i = 0;
-                }
-            }
         }
     }
 }
