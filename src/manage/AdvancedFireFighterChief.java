7d764f9f1de9de5ed621378724f0feb24dcea66e
eyraudh <Hadrien.eyraud@grenoble-inp.org>
2022-11-01 17:59:15 +0100

    feat: adding AdvancedFireFighterChief.java

--- src/manage/AdvancedFireFighterChief.java
+++ src/manage/AdvancedFireFighterChief.java
@@ -1,6 +1,59 @@
 package manage;
 
-public class AdvancedFireFighterChief {
-    public AdvancedFireFighterChief() {
+import exceptions.NotNeighboringCasesException;
+import exceptions.UnknownDirectionException;
+import exceptions.UnreachableCaseException;
+import pathfinding.Path;
+import robot.Robot;
+import simu.Incendie;
+import terrain.Carte;
+
+import java.util.*;
+
+public class AdvancedFireFighterChief extends FireFighterChief {
+    private final int incendiesSize;
+
+    public AdvancedFireFighterChief(ArrayList<Robot> robots, ArrayList<Incendie> incendies, Carte carte) {
+        super(robots, incendies, carte);
+        this.incendiesSize = incendies.size();
+    }
+
+    @Override
+    public void affectRobot() throws UnknownDirectionException, UnreachableCaseException, NotNeighboringCasesException {
+        for (int j = 0; j < incendiesSize; j++) {
+            Incendie incendie = incendies.get(j);
+            if (incendie.getNbL() > 0) {
+                Robot fastestRobot = null;
+                Path minPath = null;
+                int minTime = 0;
+                for (int i = 0; i < robotsSize; i++) {
+                    Robot curRobot = robots.get(i);
+                    if (curRobot.isWaiting() && curRobot.isAccessible(incendie.getFireCase())) {
+                        Path path = curRobot.aStar(carte, curRobot.getPosition(), incendie.getFireCase());
+                        if (fastestRobot == null) {
+                            minTime = path.getDuration();
+                            fastestRobot = curRobot;
+                            minPath = path;
+                            continue;
+                        }
+                        if (path.getDuration() < minTime) {
+                            fastestRobot = curRobot;
+                            minPath = path;
+                        }
+                    }
+                }
+                if (fastestRobot != null) {
+                    fastestRobot.followPath(minPath, carte);
+                }
+            }
+            if (incendie.getNbL() == 0) {
+                incendies.remove(j);
+                j--;
+            }
+            if (j == incendiesSize - 1 && !incendies.isEmpty()) {
+                j = 0;
+            }
+
+        }
     }
 }
--- src/manage/FireFighterChief.java
+++ src/manage/FireFighterChief.java
@@ -1,5 +1,8 @@
 package manage;
 
+import exceptions.NotNeighboringCasesException;
+import exceptions.UnknownDirectionException;
+import exceptions.UnreachableCaseException;
 import robot.Robot;
 import simu.Incendie;
 import terrain.Carte;
@@ -9,12 +12,16 @@ import java.util.ArrayList;
 public abstract class FireFighterChief {
     protected ArrayList<Robot> robots;
     protected ArrayList<Incendie> incendies;
+    protected int robotsSize;
     protected Carte carte;
 
     public FireFighterChief(ArrayList<Robot> robots, ArrayList<Incendie> incendies, Carte carte) {
         this.robots = robots;
         this.incendies = incendies;
         this.carte = carte;
+        this.robotsSize = robots.size();
     }
+
+    public abstract void affectRobot() throws UnknownDirectionException, UnreachableCaseException, NotNeighboringCasesException;
 
 }
