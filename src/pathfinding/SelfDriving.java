55e52f70917bcaf81648864d00ca0528a9f25c5b
eyraudh <Hadrien.eyraud@grenoble-inp.org>
2022-11-03 19:16:15 +0100

    feat: prototype of Dijkstra algorithm

--- src/pathfinding/SelfDriving.java
+++ src/pathfinding/SelfDriving.java
@@ -1,5 +1,6 @@
 package pathfinding;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.PriorityQueue;
@@ -11,6 +12,7 @@ import terrain.Case;
 
 /**
  * A class implementing the ability of finding the closest way to a given Case.
+ * TODO : replace heuristic by dist.
  */
 public abstract class SelfDriving {
 
@@ -27,6 +29,18 @@ public abstract class SelfDriving {
             this.position = position;
             this.cost = 0;
             this.heuristic = 0;
+            this.previous = null;
+        }
+
+        /**
+         * New constructor for the dijkstra algorithm
+         *
+         * @param position
+         * @param cost
+         */
+        public Node(Case position, int dist) {
+            this.position = position;
+            this.heuristic = dist;
             this.previous = null;
         }
 
@@ -65,10 +79,8 @@ public abstract class SelfDriving {
 
         @Override
         public boolean equals(Object o) {
-            if (o instanceof Node)
-                return this.position == ((Node) o).getPosition();
-            else
-                return false;
+            if (o instanceof Node) return this.position == ((Node) o).getPosition();
+            else return false;
         }
     }
 
@@ -76,11 +88,13 @@ public abstract class SelfDriving {
 
     /**
      * Return the speed of the robot (or anything SelfDriving) on a given Case.
-     * 
+     *
      * @param place The Case where we want to get the speed.
      * @return Speed of this on place.
      */
     public abstract int getSpeedOn(Case place);
+
+    protected abstract boolean findWater(Case place);
 
     public abstract int getTimeOn(Case pos);
 
@@ -95,9 +109,10 @@ public abstract class SelfDriving {
         return node;
     }
 
+
     /**
      * Implementation of A* algorithm for pathfinding.
-     * 
+     *
      * @param carte       Carte of the simulation.
      * @param origin      Case from where we go.
      * @param destination Case where we want to go.
@@ -105,8 +120,7 @@ public abstract class SelfDriving {
      * @throws UnreachableCaseException
      * @throws NotNeighboringCasesException
      */
-    public Path aStar(Carte carte, Case origin, Case destination)
-            throws UnreachableCaseException, NotNeighboringCasesException {
+    public Path aStar(Carte carte, Case origin, Case destination) throws UnreachableCaseException, NotNeighboringCasesException {
         graph = new HashMap<Case, Node>();
         HashSet<Node> closedList = new HashSet<>();
         PriorityQueue<Node> openList = new PriorityQueue<Node>();
@@ -122,10 +136,7 @@ public abstract class SelfDriving {
                     Node v = getNode(position);
                     if (!(closedList.contains(v) || (openList.contains(v) && v.getCost() < u.getCost()))) {
                         v.setCost(u.getCost() + carte.getTailleCases() / (this.getSpeedOn(u.getPosition())));
-                        v.setHeuristic(
-                                v.cost +
-                                        (int) (Math.pow(position.getLigne() - destination.getLigne(), 2) +
-                                                Math.pow(position.getColonne() - destination.getColonne(), 2)));
+                        v.setHeuristic(v.cost + (int) (Math.pow(position.getLigne() - destination.getLigne(), 2) + Math.pow(position.getColonne() - destination.getColonne(), 2)));
                         openList.add(v);
                         v.setPrevious(u);
                     }
@@ -138,7 +149,7 @@ public abstract class SelfDriving {
 
     /**
      * Generate path from Node.
-     * 
+     *
      * @param carte Carte of the simulation.
      * @param node  End of the path.
      * @return Path
@@ -153,30 +164,68 @@ public abstract class SelfDriving {
         }
         return path;
     }
+
+
+    public Path Dijsktra(Carte carte, Case origin, ArrayList<Case> refillCases) {
+        /* Initialization of the graph */
+        Node bestWaterNode = null;
+        PriorityQueue<Node> vertexPriorityQueue = new PriorityQueue<Node>();
+        graph = new HashMap<Case, Node>();
+        for (Case place : carte.getCases()) {
+            if (isAccessible(place)) {
+                Node n = new Node(place, Integer.MAX_VALUE);
+                graph.put(place, n);
+                if (!place.equals(origin)) {
+                    vertexPriorityQueue.add(n);
+                }
+            }
+        }
+        graph.get(origin).setHeuristic(0);
+
+        /* Sets personnalized cost of all nodes and finds bestWaterNode */
+        while (!vertexPriorityQueue.isEmpty()) {
+            Node u = vertexPriorityQueue.poll(); /* Maybe it's not the min but max...*/
+            for (Case position : carte.getVoisins(u.getPosition())) {
+                if (isAccessible(position)) {
+                    Node v = getNode(position);
+                    int alt = u.getHeuristic() + carte.getTailleCases() / this.getSpeedOn(u.getPosition());
+                    if (alt < v.getHeuristic()) {
+                        v.setHeuristic(alt);
+                        v.setPrevious(u);
+                    }
+                }
+            }
+            if (this.findWater(u.getPosition())) {
+                if (bestWaterNode == null || bestWaterNode.getHeuristic() > u.getHeuristic()) {
+                    bestWaterNode = u;
+                }
+            }
+        }
+        return generatePath(carte, bestWaterNode);
+    }
     /**
-     * 1 function Dijkstra(Graph, source):
-     * 2 dist[source] ← 0 // Initialization
-     * 3
-     * 4 create vertex priority queue Q
-     * 5
-     * 6 for each vertex v in Graph.Vertices:
-     * 7 if v ≠ source
-     * 8 dist[v] ← INFINITY // Unknown distance from source to v
-     * 9 prev[v] ← UNDEFINED // Predecessor of v
-     * 10
-     * 11 Q.add_with_priority(v, dist[v])
-     * 12
-     * 13
-     * 14 while Q is not empty: // The main loop
-     * 15 u ← Q.extract_min() // Remove and return best vertex
-     * 16 for each neighbor v of u: // Go through all v neighbors of u
-     * 17 alt ← dist[u] + Graph.Edges(u, v)
-     * 18 if alt < dist[v]:
-     * 19 dist[v] ← alt
-     * 20 prev[v] ← u
-     * 21 Q.decrease_priority(v, alt)
-     * 22
-     * 23 return dist, prev
-     *
+     1  function Dijkstra(Graph, source):
+     2      dist[source] ← 0                           // Initialization
+     3
+     4      create vertex priority queue Q
+     5
+     6      for each vertex v in Graph.Vertices:
+     7          if v ≠ source
+     8              dist[v] ← INFINITY                 // Unknown distance from source to v
+     9              prev[v] ← UNDEFINED                // Predecessor of v
+     10
+     11         Q.add_with_priority(v, dist[v])
+     12
+     13
+     14     while Q is not empty:                      // The main loop
+     15         u ← Q.extract_min()                    // Remove and return best vertex
+     16         for each neighbor v of u:              // Go through all v neighbors of u
+     17             alt ← dist[u] + Graph.Edges(u, v)
+     18             if alt < dist[v]:
+     19                 dist[v] ← alt
+     20                 prev[v] ← u
+     21                 Q.decrease_priority(v, alt)
+     22
+     23     return prev
      */
 }
