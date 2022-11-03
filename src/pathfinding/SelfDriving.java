e2469e54ccd6777acfc013c741875b8342f7acba
Titouan Mazier <titouan.mazier@grenoble-inp.org>
2022-11-03 18:13:20 +0100

    fix: take relative speed in account in A*

--- src/pathfinding/SelfDriving.java
+++ src/pathfinding/SelfDriving.java
@@ -121,7 +121,7 @@ public abstract class SelfDriving {
                 if (isAccessible(position)) {
                     Node v = getNode(position);
                     if (!(closedList.contains(v) || (openList.contains(v) && v.getCost() < u.getCost()))) {
-                        v.setCost(u.getCost() + 1);
+                        v.setCost(u.getCost() + carte.getTailleCases() / (this.getSpeedOn(u.getPosition())));
                         v.setHeuristic(
                                 v.cost +
                                         (int) (Math.pow(position.getLigne() - destination.getLigne(), 2) +
@@ -154,29 +154,29 @@ public abstract class SelfDriving {
         return path;
     }
     /**
-     * 1  function Dijkstra(Graph, source):
-     * 2      dist[source] ← 0                           // Initialization
+     * 1 function Dijkstra(Graph, source):
+     * 2 dist[source] ← 0 // Initialization
      * 3
-     * 4      create vertex priority queue Q
+     * 4 create vertex priority queue Q
      * 5
-     * 6      for each vertex v in Graph.Vertices:
-     * 7          if v ≠ source
-     * 8              dist[v] ← INFINITY                 // Unknown distance from source to v
-     * 9              prev[v] ← UNDEFINED                // Predecessor of v
+     * 6 for each vertex v in Graph.Vertices:
+     * 7 if v ≠ source
+     * 8 dist[v] ← INFINITY // Unknown distance from source to v
+     * 9 prev[v] ← UNDEFINED // Predecessor of v
      * 10
-     * 11         Q.add_with_priority(v, dist[v])
+     * 11 Q.add_with_priority(v, dist[v])
      * 12
      * 13
-     * 14     while Q is not empty:                      // The main loop
-     * 15         u ← Q.extract_min()                    // Remove and return best vertex
-     * 16         for each neighbor v of u:              // Go through all v neighbors of u
-     * 17             alt ← dist[u] + Graph.Edges(u, v)
-     * 18             if alt < dist[v]:
-     * 19                 dist[v] ← alt
-     * 20                 prev[v] ← u
-     * 21                 Q.decrease_priority(v, alt)
+     * 14 while Q is not empty: // The main loop
+     * 15 u ← Q.extract_min() // Remove and return best vertex
+     * 16 for each neighbor v of u: // Go through all v neighbors of u
+     * 17 alt ← dist[u] + Graph.Edges(u, v)
+     * 18 if alt < dist[v]:
+     * 19 dist[v] ← alt
+     * 20 prev[v] ← u
+     * 21 Q.decrease_priority(v, alt)
      * 22
-     * 23     return dist, prev
+     * 23 return dist, prev
      *
      */
 }
