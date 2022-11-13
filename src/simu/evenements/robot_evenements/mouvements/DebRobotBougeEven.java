4c88d7267a83a74dd260a1cecfb3aa6484bf3840
Titouan Mazier <titouan.mazier@grenoble-inp.org>
2022-11-13 11:47:32 +0100

    javadoc: simu package

--- src/simu/evenements/robot_evenements/mouvements/DebRobotBougeEven.java
+++ src/simu/evenements/robot_evenements/mouvements/DebRobotBougeEven.java
@@ -5,14 +5,38 @@ import simu.Simulateur;
 import simu.evenements.robot_evenements.RobotEven;
 import terrain.Direction;
 
+/**
+ * Order a robot to start a move toward given direction.
+ */
 public class DebRobotBougeEven extends RobotEven {
     Direction dir;
 
+    /**
+     * Creates an event for starting the robot's movement.
+     * 
+     * It is a manual event, meaning it is kept in history for restart.
+     * 
+     * @param date       Date of the execution.
+     * @param sim        Running simulation.
+     * @param robotIndex The index of the robot in DonneesSimulation::getRobots()
+     *                   that will interevene.
+     * @param dir        Direction toward which to move the robot.
+     */
     public DebRobotBougeEven(long date, Simulateur sim, int robotIndex, Direction dir) {
         super(date, sim, robotIndex);
         this.dir = dir;
     }
 
+    /**
+     * Creates an event for starting the robot's movement.
+     * 
+     * It is an automatic event, meaning it is not kept in history for restart.
+     * 
+     * @param date  Date of the execution.
+     * @param sim   Running simulation.
+     * @param robot Robot to move.
+     * @param dir   Direction toward which to move the robot.
+     */
     public DebRobotBougeEven(long date, Simulateur sim, Robot robot, Direction dir) {
         super(date, sim, robot);
         this.dir = dir;
